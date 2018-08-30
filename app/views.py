e8d5286ebdca49143b4e6e75f31cc4ba4da36df5
damunza <danidamunza@gmail.com>
2018-08-30 17:16:23 +0300

    called the function get articles

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from app import app
 
-from .request import get_source
+from .request import get_source, get_article
 
 @app.route('/')
 def index():
@@ -19,4 +19,7 @@ def article(article_id):
     '''
     function that returns the article.html page and its contect
     '''
-    return render_template('article.html',id = article_id)
+    article = get_article(article_id)
+    print(article)
+    title = f'{article_id}'
+    return render_template('article.html',id = article_id,title = title,article = article)
