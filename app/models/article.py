443421f462ff20e3caab09f071f9684b2f0a860f
damunza <danidamunza@gmail.com>
2018-08-30 17:17:55 +0300

    ran the app and it transitions from the index to the article pages and displays articles

---
+++ app/models/article.py
@@ -0,0 +1,8 @@
+class Article:
+    def __init__(self,author,title,description,url,urlToImage,publishedAt):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.image = urlToImage
+        self.time = publishedAt
