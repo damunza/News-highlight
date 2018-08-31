ca0e62865e9781976ecace3e15b12bea0ef79413
damunza <danidamunza@gmail.com>
2018-08-31 18:56:17 +0300

    changed run.py to manage.py

--- app/models/article.py
+++
@@ -1,8 +0,0 @@
-class Article:
-    def __init__(self,author,title,description,url,urlToImage,publishedAt):
-        self.author = author
-        self.title = title
-        self.description = description
-        self.url = url
-        self.image = urlToImage
-        self.time = publishedAt
--- app/models/source.py
+++
@@ -1,6 +0,0 @@
-class Source :
-    def __init__(self,id,name,description,language):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.language = language
