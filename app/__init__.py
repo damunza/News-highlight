9475fff097ee44c0be28f6ad9bb03c4c6d3a73e2
damunza <danidamunza@gmail.com>
2018-08-31 11:57:44 +0300

    imported the flask bootstrap modules

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from .config import DevConfig
 
 
 #instance of flask
 app = Flask(__name__, instance_relative_config = True)
 
-#after importin Devconfig
+#after importing Devconfig
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+#making an instance of bootstrap
+bootstrap = Bootstrap(app)
 
 from app import views
